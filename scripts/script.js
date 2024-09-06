a5ca36c3e3446b1c111401f9bb7ea3afd46a48b1
Kurt Secretario <secretariokurt3219@gmail.com>
2024-09-06 18:10:05 +0800

    <refactor>: make button var into single var

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,5 @@
-// Script here...
-
 // Output screen
-const output_screen = document.querySelector("#output-screen");
-
+const output_screen = document.querySelector("#output_screen > p");
 // Buttons
-// Numbers
-const one_btn = document.querySelector("#one_btn");
-const two_btn = document.querySelector("#two_btn");
-const three_btn = document.querySelector("#three_btn");
-const four_btn = document.querySelector("#four_btn");
-const five_btn = document.querySelector("#five_btn");
-const six_btn = document.querySelector("#six_btn");
-const seven_btn = document.querySelector("#seven_btn");
-const eight_btn = document.querySelector("#eight_btn");
-const nine_btn = document.querySelector("#nine_btn");
-const zero_btn = document.querySelector("#zero_btn");
+const buttons = document.querySelectorAll(".btn_class");
 
-const point_btn = document.querySelector("#point_btn");
-
-// Operators
-const plus_btn = document.querySelector("#plus_btn");
-const minus_btn = document.querySelectors("#minus_btn");
-const mult_btn = document.querySelector("#mult_btn");
-const div_btn = document.querySelector("#div_btn");
-const equal_btn = document.querySelector("#equal_btn");
-
-// Clear and delete
-const delete_btn = document.querySelector("#delete_btn");
-const clear_btn = document.querySelector("#clear_btn");
