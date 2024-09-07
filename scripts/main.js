ba1a828927633294713a153d8b83f4c08dd78dac
Kurt Secretario <secretariokurt3219@gmail.com>
2024-09-07 21:45:58 +0800

    <feat>: add evaluate function to calculate adjacent operands

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,19 @@ const buttons = document.querySelectorAll(".btn_class");
 
 // Click handler for buttons
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        appendToOutput(button.value);
-    });
+  button.addEventListener("click", () => {
+      appendToOutput(button.value);
+  });
 });
 
 // Clear button handler
 clear_btn.addEventListener("click", () => {
-    clearOutput();
+  clearOutput();
 });
 
 // Delete button handler
 delete_btn.addEventListener("click", () => {
-    deleteOutput();
+  deleteOutput();
 });
 
 // Equal button handler
@@ -29,55 +29,87 @@ equal_btn.addEventListener("click", () => {
 
 // Keyboard handler
 document.addEventListener("keydown", (event) => {
-    // Map keys values to pressed keys
-    const map = {
-        0: "0", 1: "1", 2: "2", 3: "3", 4: "4",
-        5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
-        ".": ".", "+": "+", "-": "-", "*": "*",
-        "/": "/", Enter: "=", Backspace: "delete",
-        Escape: "clear",
-    };
+  // Map keys values to pressed keys
+  const map = {
+      0: "0", 1: "1", 2: "2", 3: "3", 4: "4",
+      5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
+      ".": ".", "+": "+", "-": "-", "*": "*",
+      "/": "/", Enter: "=", Backspace: "delete",
+      Escape: "clear",
+  };
 
-    const key = event.key;
-    const value = map[key];
-    if (!value) {
-        return;
-    } else if (value === "delete") {
-        deleteOutput();
-        return;
-    } else if (value === "clear") {
-        clearOutput();
-        return;
-    } else if (value === "=") {
-        // Calculate
-        return;
-    } 
+  const key = event.key;
+  const value = map[key];
+  if (!value) {
+      return;
+  } else if (value === "delete") {
+      deleteOutput();
+      return;
+  } else if (value === "clear") {
+      clearOutput();
+      return;
+  } else if (value === "=") {
+      // Calculate
+      return;
+  } 
 
-    appendToOutput(value);
+  appendToOutput(value);
 });
 
 // UTILITY FUNCTIONS
 
 function appendToOutput(value) {
-    // Prevent consecutive operators
-    const isNum = /^[0-9]$/;
-    if (
-        !isNum.test(output_screen.textContent.slice(-1)) &&
-        isOperator(value)
-    ) {
-        return;
-    }
-    output_screen.textContent += value;
+  // Prevent consecutive operators
+  const isNum = /^[0-9]$/;
+  if (
+      !isNum.test(output_screen.textContent.slice(-1)) &&
+      isOperator(value)
+  ) {
+      return;
+  }
+  output_screen.textContent += value;
 }
 
 function clearOutput() {
-    output_screen.textContent = "";
+  output_screen.textContent = "";
 }
 
 function deleteOutput() {
-    output_screen.textContent = output_screen.textContent.slice(0, -1);
+  output_screen.textContent = output_screen.textContent.slice(0, -1);
 }
 
 function isOperator(op) {
-    return ["+", "-", "*", "/"].includes(op);
+  return ["+", "-", "*", "/"].includes(op);
+}
+
+function precedence(op) {
+  switch (op) {
+    case "*":
+    case "/":
+      return 2;
+    case "+":
+    case "-":
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+// Calculate adjacent operands
+function evaluate(oprnd_1, oprnd_2, op) {
+  switch (op) {
+    case "+":
+      return oprnd_1 + oprnd_2;
+    case "-":
+      return oprnd_2 - oprnd_1;
+    case "*":
+      return oprnd_1 * oprnd_2;
+    case "/":
+      if (oprnd_2 === 0) {
+        throw new Error("Division by zero");
+      }
+      return oprnd_2 / oprnd_1;
+    default:
+      throw new Error("Invalid operator");
+  }
 }
